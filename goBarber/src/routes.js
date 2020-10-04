751485ff609b8ef42de842847fab2f0432d1fd06
mathcard <matheus.carmo@hotmail.com>
2020-10-04 20:22:04 -0300

    Finalizando o projeto gobarber

--- goBarber/src/routes.js
+++ goBarber/src/routes.js
@@ -8,6 +8,10 @@ const guestMiddleware = require('./app/middlewares/guest')
 
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionsController')
+const DashboardController = require('./app/controllers/DashboardController')
+const FileController = require('./app/controllers/FileController')
+const AppointmentController = require('./app/controllers/AppointmentController')
+const AvailableController = require('./app/controllers/AvailableController')
 
 // Liberando as mensagens de flash para todas página njk
 routes.use((req, res, next) => {
@@ -15,6 +19,9 @@ routes.use((req, res, next) => {
   res.locals.flashError = req.flash('error')
   return next()
 })
+
+// Rota que busca arquivos
+routes.get('/files/:file', FileController.show)
 
 routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
@@ -28,9 +35,11 @@ routes.use('/app', authMiddleware)
 routes.get('/app/logout', SessionController.destroy)
 
 // Poderia adidionar a autencação adicionando o middleware como segundo parametro
-routes.get('/app/dashboard', (req, res) => {
-  console.log(req.session.user)
-  return res.render('dashboard')
-})
+routes.get('/app/dashboard', DashboardController.index)
+
+routes.get('/app/appointments/new/:provider', AppointmentController.create)
+routes.post('/app/appointments/new/:provider', AppointmentController.store)
+
+routes.get('/app/available/:provider', AvailableController.index)
 
 module.exports = routes
